b9edb8e5d75d597b6f8c43b25a2c89ac812e9a0d
DresssOrtiz <134727739+DresssOrtiz@users.noreply.github.com>
2025-04-23 18:48:04 -0500

    Create DeliveredState.java

---
+++ DeliveredState.java
@@ -0,0 +1,16 @@
+package com.restaurant.domain.state;
+
+import com.restaurant.domain.model.Order;
+
+public class DeliveredState implements OrderState {
+
+    @Override
+    public void avanzarEstado(Order order) {
+        System.out.println("El pedido ya fue entregado. No puede avanzar más.");
+    }
+
+    @Override
+    public String getNombreEstado() {
+        return "Entregado";
+    }
+}
