39f73a87278648981924de288c1a9b68e5a6e3b5
DresssOrtiz <134727739+DresssOrtiz@users.noreply.github.com>
2025-04-22 20:26:57 -0500

    Update MenuService.java
    llene el archivo, falta revision, tarea 2

--- MenuService.java
+++ MenuService.java
@@ -1,1 +1,30 @@
+package com.restaurant.application;
+
+import com.restaurant.domain.model.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuService {
+    private List<MenuItem> menu;
+
+    public MenuService() {
+        this.menu = new ArrayList<>();
+    }
+
+    public void agregarItem(MenuItem item) {
+        menu.add(item);
+    }
+
+    public List<MenuItem> obtenerMenu() {
+        return menu;
+    }
+
+    public void mostrarMenu() {
+        System.out.println("===== MENÚ DEL RESTAURANTE =====");
+        for (MenuItem item : menu) {
+            System.out.println("- " + item.getNombre() + " | $" + item.getPrecio() + " | " + item.getDescripcion());
+        }
+    }
+}
 
