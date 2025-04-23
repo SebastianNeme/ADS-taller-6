73fe9f9fe5995e50cfdd7abe9b8a77c0f4f38d3b
DresssOrtiz <134727739+DresssOrtiz@users.noreply.github.com>
2025-04-22 20:27:42 -0500

    Update RestaurantConsoleApp.java
    Llene el archivo, falta revision, tarea 2

--- RestaurantConsoleApp.java
+++ RestaurantConsoleApp.java
@@ -1,1 +1,22 @@
+package com.restaurant.ui;
+
+import com.restaurant.application.MenuService;
+import com.restaurant.infrastructure.factory.*;
+import com.restaurant.domain.model.MenuItem;
+
+public class RestaurantConsoleApp {
+    public static void main(String[] args) {
+        MenuService menuService = new MenuService();
+
+        MenuItemFactory platoFactory = new PlatoFactory("Hamburguesa Clásica", 15000, "Con papas a la francesa");
+        MenuItemFactory bebidaFactory = new BebidaFactory("Jugo Natural", 5000, "Jugo de mango sin azúcar");
+        MenuItemFactory postreFactory = new PostreFactory("Flan de coco", 7000, "Hecho en casa");
+
+        menuService.agregarItem(platoFactory.crearItem());
+        menuService.agregarItem(bebidaFactory.crearItem());
+        menuService.agregarItem(postreFactory.crearItem());
+
+        menuService.mostrarMenu();
+    }
+}
 
