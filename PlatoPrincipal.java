caa4d1be2e89fa1e754686974444a04d9fcedec2
DresssOrtiz <134727739+DresssOrtiz@users.noreply.github.com>
2025-04-22 20:20:34 -0500

    Create PlatoPrincipal.java
    Cree el archivo, falta revision, tarea 2

---
+++ PlatoPrincipal.java
@@ -0,0 +1,22 @@
+package com.restaurant.domain.model;
+
+public class PlatoPrincipal implements MenuItem {
+    private String nombre;
+    private double precio;
+    private String descripcion;
+
+    public PlatoPrincipal(String nombre, double precio, String descripcion) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String getNombre() { return nombre; }
+
+    @Override
+    public double getPrecio() { return precio; }
+
+    @Override
+    public String getDescripcion() { return descripcion; }
+}
