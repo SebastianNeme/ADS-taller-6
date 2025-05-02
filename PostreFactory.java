5617b02170b3e7afdd59af75f20c8cdc3f9e6968
SebastianNeme <s_peralta@javeriana.edu.co>
2025-05-02 05:29:32 -0500

    Update PostreFactory.java

--- PostreFactory.java
+++ PostreFactory.java
@@ -1,21 +1,12 @@
-package com.restaurant.infrastructure.persistence;
+package com.restaurant.domain.factory;
 
-import com.restaurant.domain.model.Postre;
 import com.restaurant.domain.model.MenuItem;
+import com.restaurant.domain.model.Postre;
 
 public class PostreFactory implements MenuItemFactory {
-    private String nombre;
-    private double precio;
-    private String descripcion;
-
-    public PostreFactory(String nombre, double precio, String descripcion) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.descripcion = descripcion;
-    }
 
     @Override
-    public MenuItem crearItem() {
-        return new Postre(nombre, precio, descripcion);
+    public MenuItem crear(String nombre, double precio) {
+        return new Postre(nombre, precio);
     }
 }
