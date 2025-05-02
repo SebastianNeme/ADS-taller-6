8f71534deab94b661b5e4bb46eff09f30174884e
SebastianNeme <s_peralta@javeriana.edu.co>
2025-05-02 05:30:45 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,20 @@
 
+package com.restaurant.domain.model;
+
+public class Customer {
+    private String nombre;
+    private String contacto;
+
+    public Customer(String nombre, String contacto) {
+        this.nombre = nombre;
+        this.contacto = contacto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+}
