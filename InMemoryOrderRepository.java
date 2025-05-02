b3342cb00b336a81cb08ac06a7d82d9af859c596
SebastianNeme <s_peralta@javeriana.edu.co>
2025-05-02 05:31:38 -0500

    Update InMemoryOrderRepository.java

--- InMemoryOrderRepository.java
+++ InMemoryOrderRepository.java
@@ -1,1 +1,33 @@
+package com.restaurant.infrastructure;
+
+import com.restaurant.domain.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryOrderRepository {
+
+    private List<Order> pedidos = new ArrayList<>();
+
+    public void guardar(Order pedido) {
+        pedidos.add(pedido);
+    }
+
+    public List<Order> obtenerTodos() {
+        return pedidos;
+    }
+
+    public Order buscarPorId(int id) {
+        for (Order p : pedidos) {
+            if (p.getId() == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void limpiar() {
+        pedidos.clear();
+    }
+}
 
